c01d967f04c43f74fe8751fbacf0f29e48d0e091
TagKrj <trangbui19032004@gmail.com>
2024-06-09 21:34:09 +0700

    first commit

---
+++ webTNJ/TNJ2home.php
@@ -0,0 +1,450 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TNJ Jewelry</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="TNJ2home.css">
+    <!-- font chữ -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+
+    <!-- ShopNow -->
+    <link rel="stylesheet" href="ShopNow1.php">
+    <link rel="stylesheet" href="SignUp&Login.php">
+</head>
+
+<body>
+    <!-- header section starts -->
+    <header>
+
+        <input type="checkbox" name="" id="toggler">
+        <label for="toggler" class="fas fa-bars"></label>
+
+        <a href="#Trangchu" class="logo"><img style="height: 8rem;" src="lg1jpg.jpg" alt=""></a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#about">about</a>
+            <a href="#products">products</a>
+            <a href="#review">review</a>
+            <a href="#contact">contact</a>
+        </nav>
+
+        <div class="icons">
+            <a href="#" class="fas fa-heart"></a>
+            <a href="ShoppingCart.php" class="fas fa-shopping-cart"></a>
+            <a href="SignUp&Login.php" class="fas fa-user"></a>
+
+        </div>
+
+    </header>
+
+    <!-- header section end -->
+    <!-- home section starts -->
+    <section class="home" id="home">
+
+        <div class="content">
+            <h3>TNJ Jewelry</h3>
+            <span>Durable & beautiful jewelry</span>
+            <p> Adorned with exquisite gemstones and crafted with impeccable artistry, these jewels exude timeless
+                elegance and refined luxury.</p>
+            <p>
+                Embrace the epitome of style and sophistication with these captivating pieces of jewelry, where timeless
+                elegance meets modern trends.</p>
+            <a href="ShopNow1.php" class="btn">shop now</a>
+        </div>
+
+
+    </section>
+    <!-- home section end -->
+
+    <!-- about section starts -->
+
+    <section class="about" id="about">
+
+        <h1 class="heading"><span> about </span>us</h1>
+
+        <div class="row">
+
+            <div class="video-container">
+                <video src="vd2.mp4" loop autoplay muted></video>
+                <h3>best jewelry sellers</h3>
+            </div>
+
+            <div class="content">
+
+                <h3>why choose us?</h3>
+                <p>Since its establishment in 2024, TNJ has been offering exquisite jewelry designs that embody timeless
+                    elegance and purity for women of all ages. Each meticulously handcrafted piece from TNJ is a perfect
+                    blend of classic and contemporary aesthetics, adding a touch of refinement to every woman's unique
+                    style.</p>
+
+                <p>
+                    With a diverse range of designs to suit every occasion, TNJ is committed to providing customers with
+                    high-quality jewelry and exceptional service.Step into the world of TNJ and discover exquisite
+                    jewelry that speaks to your style and elevates your
+                    presence.</p>
+
+                <a href="#" class="btn">learn more</a>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <!-- about section ends -->
+
+    <!-- icons section starts -->
+
+    <section class="icons-container">
+
+        <div class="icons">
+            <img src="ic1.png" alt="">
+            <div class="info">
+                <h3>free delivery</h3>
+                <span>on all order</span>
+            </div>
+        </div>
+
+        <div class="icons">
+            <img src="ic2 (1).png" alt="">
+            <div class="info">
+                <h3>10 days returns</h3>
+                <span>moneyback ganatee</span>
+            </div>
+        </div>
+
+        <div class="icons">
+            <img src="ic3.png" alt="">
+            <div class="info">
+                <h3>offer & gifts</h3>
+                <span>on all order</span>
+            </div>
+        </div>
+
+        <div class="icons">
+            <img src="ic4.png" alt="">
+            <div class="info">
+                <h3>secure paymens</h3>
+                <span>protected by paypal</span>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- icons section ends -->
+
+    <!-- products section starts -->
+
+    <section class="products" id="products">
+        <h1 class="heading">latest <span>products</span></h1>
+        <div class="box-container">
+
+            <div class="box">
+                <span class="discount">-50%</span>
+                <div class="image">
+                    <img src="https://lili.vn/wp-content/uploads/2022/08/Mat-day-chuyen-bac-nu-dinh-da-CZ-co-4-la-LILI_426865_31.jpg" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>ruby necklace</h3>
+                    <div class="price">$102.99 <span>$255.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://www.lialijewellery.com/pub/media/catalog/product/cache/b7b7e9cf453f82c940a1ce0323c1d64b/s/n/snk0001636.jpg" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>Diamond Necklace</h3>
+                    <div class="price">$399.99 <span>$550.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-30%</span>
+                <div class="image">
+                    <img src="https://lili.vn/wp-content/uploads/2022/07/Nhan-bac-nu-dinh-kim-cuong-Moissanite-Aidan-LILI_335168_1-400x400.jpg" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>ring</h3>
+                    <div class="price">$87.99 <span>$120.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://lili.vn/wp-content/uploads/2021/01/Nhan-bac-ma-vang-Cay-o-liu-LILI_114577-05-400x400.jpg" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>gold Ring</h3>
+                    <div class="price">$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://static.wixstatic.com/media/11062b_2b898d5ce40e435eaf8bcf3c3261ea78~mv2.jpg/v1/fill/w_1356,h_1356,q_90/11062b_2b898d5ce40e435eaf8bcf3c3261ea78~mv2.webp" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://static.wixstatic.com/media/11062b_d0ce180c5355448c988532b04913d4dd~mv2.jpg/v1/fill/w_1356,h_1356,q_90/11062b_d0ce180c5355448c988532b04913d4dd~mv2.webp" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://static.wixstatic.com/media/11062b_3c54849b406248e69723ffe244135d4b~mv2.jpg/v1/fill/w_1470,h_1470,q_90/11062b_3c54849b406248e69723ffe244135d4b~mv2.webp" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="https://lili.vn/wp-content/uploads/2022/06/Nhan-cap-doi-bac-dinh-kim-cuong-Moissanite-Theophilus-LILI_672438_4.jpg" alt="">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    </div>
+                </div>
+
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">$12.99 <span>$15.99</span></div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <!-- products section ends -->
+
+    <!-- review section starts -->
+
+    <section class="review" id="review">
+
+        <h1 class="heading">customer's <span>review</span></h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa natus iste aperiam delectus eum! Velit
+                    laborum nostrum libero. Ab voluptatibus harum vero corrupti possimus reiciendis, vitae quia sapiente
+                    qui ad?</p>
+
+                <div class="user">
+                    <img src="https://png.pngtree.com/thumb_back/fh260/background/20221128/pngtree-outdoorsy-young-woman-sporting-shades-confidently-posing-with-a-juice-bottle-meets-the-camera-gaze-photo-image_42709010.jpg" alt="">
+                    <div class="user-info">
+                        <h3>John deo</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+
+                <span class="fas fa-quote-right"></span>
+            </div>
+
+            <div class="box">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa natus iste aperiam delectus eum! Velit
+                    laborum nostrum libero. Ab voluptatibus harum vero corrupti possimus reiciendis, vitae quia sapiente
+                    qui ad?</p>
+
+                <div class="user">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2goftFJBbe8xqqJlplYIxPXa2QFsVsLcSPmPM4IJz4AuHVzRdnrFJTSMKGk-b_PR9OS0&usqp=CAU" alt="">
+                    <div class="user-info">
+                        <h3>John deo</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+
+                <span class="fas fa-quote-right"></span>
+            </div>
+
+            <div class="box">
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa natus iste aperiam delectus eum! Velit
+                    laborum nostrum libero. Ab voluptatibus harum vero corrupti possimus reiciendis, vitae quia sapiente
+                    qui ad?</p>
+
+                <div class="user">
+                    <img src="https://letranlaw.com/wp-content/uploads/2020/02/hannahavatar2.jpg" alt="">
+                    <div class="user-info">
+                        <h3>John deo</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+
+                <span class="fas fa-quote-right"></span>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- review section ends -->
+
+
+    <!-- contact section starts -->
+
+    <section class="contact" id="contact">
+
+        <h1 class="heading"> <span>contact</span> us</h1>
+
+        <div class="row">
+
+            <form action="">
+                <input type="text" placeholder="name" class="box">
+                <input type="email" placeholder="email" class="box">
+                <input type="number" placeholder="number" class="box">
+                <textarea name="" class="box" placeholder="message" id="" cols="30" rows="10"></textarea>
+                <input type="submit" value="send message" class="btn">
+            </form>
+
+            <div class="image">
+                <img src="https://kimcuongdanang.com/wp-content/uploads/2022/07/Gems-Casa-bai-viet-phoi-trang-suc-quan-ao-5.jpg" alt="">
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- contact section ends -->
+
+    <!-- footer section starts -->
+
+    <section class="footer">
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>quick links</h3>
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#products">products</a>
+                <a href="#review">review</a>
+                <a href="#contact">contact</a>
+            </div>
+
+            <div class="box">
+                <h3>extra links</h3>
+                <a href="">my acccount</a>
+                <a href="">my order</a>
+                <a href="">my favorite</a>
+            </div>
+
+            <div class="box">
+                <h3>locations</h3>
+                <a href="">india</a>
+                <a href="">USA</a>
+                <a href="">japan</a>
+                <a href="">france</a>
+            </div>
+
+            <div class="box">
+                <h3>contact info</h3>
+                <a href="">+84-9887654</a>
+                <a href="">TNJ@gmail.com</a>
+                <a href="">Triều Khúc - Thanh Xuân - Hà Nội</a>
+                <img src="cardpayjpg.jpg" alt="">
+            </div>
+
+        </div>
+
+        <div class="credit"> created by <span> WE </span>|Bùi Hiền Trang, Phan Văn Tùng, Ngô Thị Thảo Thơm, Vũ Quang
+            Minh, Nguyễn Văn Cường</div>
+
+    </section>
+
+    <!-- footer section ends -->
+
+
+</body>
+
+</html>
